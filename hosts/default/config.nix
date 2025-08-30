682b90a669da8c8aebb37f891319bca0bbf19545
Hudson Bradley <megastrik3@gmail.com>
2025-08-29 20:00:12 -0400

    remove default security config

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -285,31 +285,31 @@
   };
 
   # Security / Polkit
-  security = { 
-    rtkit.enable = true;
-    polkit.enable = true;
-    polkit.extraConfig = ''
-     polkit.addRule(function(action, subject) {
-       if (
-         subject.isInGroup("users")
-           && (
-             action.id == "org.freedesktop.login1.reboot" ||
-             action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-             action.id == "org.freedesktop.login1.power-off" ||
-             action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-           )
-         )
-       {
-         return polkit.Result.YES;
-       }
-    })
-  '';
- };
-  security.pam.services.swaylock = {
-    text = ''
-      auth include login
-    '';
-  };
+#   security = { 
+#     rtkit.enable = true;
+#     polkit.enable = true;
+#     polkit.extraConfig = ''
+#      polkit.addRule(function(action, subject) {
+#        if (
+#          subject.isInGroup("users")
+#            && (
+#              action.id == "org.freedesktop.login1.reboot" ||
+#              action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+#              action.id == "org.freedesktop.login1.power-off" ||
+#              action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+#            )
+#          )
+#        {
+#          return polkit.Result.YES;
+#        }
+#     })
+#   '';
+#  };
+#   security.pam.services.swaylock = {
+#     text = ''
+#       auth include login
+#     '';
+#   };
 
 
   security = {
