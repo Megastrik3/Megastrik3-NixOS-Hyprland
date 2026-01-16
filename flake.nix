dff4bbed60593cddc08a58b5d466631b06a103c4
megastrik3 <megastrik3@gmail.com>
2026-01-15 19:40:47 -0500

    added noctilia shell

--- flake.nix
+++ flake.nix
@@ -3,7 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+      # Follow corresponding `release` branch from Home Manager
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     quickshell = {
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -50,6 +58,7 @@
           modules = [
             ./hosts/${host}/config.nix
             ./modules/quickshell.nix
+            ./noctilia/noctilia.nix
             home-manager.nixosModules.home-manager
             {
             home-manager.useGlobalPkgs = true;
